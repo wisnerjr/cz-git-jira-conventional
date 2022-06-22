d683117fcd181c9b0c5cd847e2c7e00e583486a7
Wisner Júnior <wisnerjr@users.noreply.github.com>
2022-06-21 23:46:29 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name="cz_github_jira_conventional",
+    name="cz_git_jira_conventional",
     version="1.0.0",
-    py_modules=["cz_github_jira_conventional"],
-    author="Falko Krause, apheris AI GmbH",
-    author_email="f.krause@apheris.com",
+    py_modules=["cz_git_jira_conventional"],
+    author="Wisner Júnior",
+    author_email="wisner@gmail.com",
     license="MIT",
-    url="https://github.com/apheris/cz-github-jira-conventional",
+    url="https://github.com/wisnerjr/cz-git-jira-conventional/",
     install_requires=["commitizen"],
-    description="Extend the commitizen tools to create conventional commits and README that link to Jira and GitHub.",
+    description="Extend the commitizen tools to create conventional commits and README that link to Jira and Git repo.",
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
